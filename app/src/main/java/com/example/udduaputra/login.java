4f7c6b11ce97a3f30134fd93575c7b4901827123
soniadwia <soniadwi0203@gmail.com>
2020-03-23 00:18:53 +0700

    pindah layout (Activity) login ke reset_password menggunakan Intent

--- app/src/main/java/com/example/udduaputra/login.java
+++ app/src/main/java/com/example/udduaputra/login.java
@@ -2,7 +2,9 @@ package com.example.udduaputra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class login extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void LOGIN(View view) {
+        Intent intent = new Intent(login.this, reset_password.class);
+        startActivity(intent);
     }
 }
