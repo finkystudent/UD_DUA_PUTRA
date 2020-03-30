1808b73c9cf0c11380218586da5e4d00612e4227
soniadwia <soniadwi0203@gmail.com>
2020-03-30 11:49:20 +0700

    membuat halaman utama

---
+++ app/src/main/java/com/example/udduaputra/Tampilan_Utama.java
@@ -0,0 +1,14 @@
+package com.example.udduaputra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tampilan_Utama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampilan__utama);
+    }
+}
