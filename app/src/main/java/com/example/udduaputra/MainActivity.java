0628f4b800f4a197352bb93db86b1b778095567d
finkystudent <finky.student@poliwangi.ac.id>
2020-04-14 13:31:46 +0700

    membuat splashscreen

--- app/src/main/java/com/example/udduaputra/MainActivity.java
+++ app/src/main/java/com/example/udduaputra/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.udduaputra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private int waktu_loading=3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this,login.class));
+            }
+        },3000);
+
+    }}
