82516d873db884c540036c7313113146cc6b8ad0
finkystudent <finky.student@poliwangi.ac.id>
2020-05-15 17:09:57 +0700

    membuat halaman laporan aktivitas sales

---
+++ app/src/main/java/com/example/udduaputra/Lap_aktivitas.java
@@ -0,0 +1,14 @@
+package com.example.udduaputra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lap_aktivitas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lap_aktivitas);
+    }
+}
