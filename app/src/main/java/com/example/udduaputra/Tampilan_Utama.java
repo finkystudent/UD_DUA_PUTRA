3bfa22164db7e9043d702468e3aa3174f16f1f07
finkystudent <finky.student@poliwangi.ac.id>
2020-06-02 21:36:18 +0700

    pembenahan intent button

--- app/src/main/java/com/example/udduaputra/Tampilan_Utama.java
+++ app/src/main/java/com/example/udduaputra/Tampilan_Utama.java
@@ -2,7 +2,9 @@ package com.example.udduaputra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Tampilan_Utama extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Tampilan_Utama extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tampilan__utama);
+    }
+
+    public void Lap_Aktivitas(View view) {
+        Intent intent = new Intent(Tampilan_Utama.this, Lap_aktivitas.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/udduaputra/login.java
+++ app/src/main/java/com/example/udduaputra/login.java
@@ -7,17 +7,23 @@ import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
     }
-    public void LOGIN(View view) {
+
+    public void Login(View view) {
         Intent intent = new Intent(login.this, Tampilan_Utama.class);
+        startActivity(intent);
+    }
+
+    public void ResetPassword(View view) {
+        Intent intent = new Intent(login.this, reset_password.class);
         startActivity(intent);
     }
 }
