afef93c619ce4ab09335699a4702dd0c9b974f1e
finkystudent <finky.student@poliwangi.ac.id>
2020-06-30 22:15:13 +0700

    volley login

---
+++ app/src/main/java/com/example/udduaputra/User.java
@@ -0,0 +1,28 @@
+package com.example.udduaputra;
+
+public class User {
+
+    private int id;
+    private String username, email;
+
+    public User(int id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+}
--- app/src/main/java/com/example/udduaputra/login.java
+++ app/src/main/java/com/example/udduaputra/login.java
@@ -1,25 +1,158 @@
 package com.example.udduaputra;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-
 import android.content.Intent;
-import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class login extends AppCompatActivity {
+    String URLs = "http://192.168.43.133/UD-DUA-PUTRA/public/login";
+
+    EditText editTextUsername, editTextPassword;
+    ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
+        if (SharedPrefManager.getInstance(this).isLoggedIn()) {
+            finish();
+            startActivity(new Intent(this, Tampilan_Utama.class));
+        }
+
+//        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        editTextUsername = (EditText) findViewById(R.id.editText);
+        editTextPassword = (EditText) findViewById(R.id.editText4);
+
+
+        //if user presses on login
+        //calling the method login
+       Button log =  findViewById(R.id.button7);
+       log.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+
+               userLogin();
+           }
+       });
+        //if user presses on not registered
+//        findViewById(R.id.textViewRegister).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                //open register screen
+//                finish();
+//                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+//            }
+//        });
     }
 
+    private void userLogin() {
+        //first getting the values
+        final String username = editTextUsername.getText().toString();
+        final String password = editTextPassword.getText().toString();
+
+        //validating inputs
+        if (TextUtils.isEmpty(username)) {
+            editTextUsername.setError("Please enter your username");
+            editTextUsername.requestFocus();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            editTextPassword.setError("Please enter your password");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        //if everything is fine
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URLs,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressBar.setVisibility(View.GONE);
+
+                        try {
+                            //converting response to json object
+                            JSONObject obj = new JSONObject(response);
+
+                            //if no error in response
+                            if (!obj.getBoolean("error")) {
+                                Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+
+                                //getting the user from the response
+                                JSONObject userJson = obj.getJSONObject("user");
+
+                                //creating a new user object
+                                User user = new User(
+                                        userJson.getInt("id"),
+                                        userJson.getString("username"),
+                                        userJson.getString("email")
+
+                                );
+
+                                //storing the user in shared preferences
+                                SharedPrefManager.getInstance(getApplicationContext()).userLogin(user);
+
+                                //starting the profile activity
+                                finish();
+                                startActivity(new Intent(getApplicationContext(), Tampilan_Utama.class));
+                            } else {
+                                Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", username);
+                params.put("password", password);
+                return params;
+            }
+        };
+
+        VolleySingleton.getInstance(this).addToRequestQueue(stringRequest);
+    }
+
+    public void imageView5(View view) {
+        Intent intent = new Intent(login.this, Tampilan_Utama.class);
+        startActivity(intent);
+
+    }
     public void Login(View view) {
         Intent intent = new Intent(login.this, Tampilan_Utama.class);
         startActivity(intent);
+
     }
 
     public void ResetPassword(View view) {
@@ -27,3 +160,4 @@ public class login extends AppCompatActivity {
         startActivity(intent);
     }
 }
+
