d51449a24f4105bbe5715719b612b542d4da7e95
finkystudent <finky.student@poliwangi.ac.id>
2020-07-01 22:18:53 +0700

    pembenahan tampilan laporan_aktivitas

--- app/src/main/java/com/example/udduaputra/Lap_aktivitas.java
+++ app/src/main/java/com/example/udduaputra/Lap_aktivitas.java
@@ -1,82 +1,129 @@
 package com.example.udduaputra;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.app.DatePickerDialog;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class Lap_aktivitas extends AppCompatActivity {
-    private AppCompatActivity appCompatActivity;
-    private DatePickerDialog datePickerDialog;
-    private SimpleDateFormat dateFormatter;
-    private TextView tvDateResult;
-    private Button btDatePicker;
+    Calendar myCalendar;
+    DatePickerDialog.OnDateSetListener date;
+    EditText bt_datepicker;
+    TextView namanya;
+    Button simpan;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lap_aktivitas);
 
-        /**
-         * Kita menggunakan format tanggal dd-MM-yyyy
-         * jadi nanti tanggal nya akan diformat menjadi
-         * misalnya 01-12-2017
-         */
-        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+        bt_datepicker = (EditText) findViewById(R.id.bt_datepicker);
+        namanya = findViewById(R.id.add_nama);
+        simpan = findViewById(R.id.add_simpan);
 
-        tvDateResult = (TextView) findViewById(R.id.tv_dateresult);
-        btDatePicker = (Button) findViewById(R.id.bt_datepicker);
-        btDatePicker.setOnClickListener(new View.OnClickListener() {
+        myCalendar = Calendar.getInstance();
+        date = new DatePickerDialog.OnDateSetListener() {
             @Override
-            public void onClick(View view) {
-                showDateDialog();
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMounth) {
+
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMounth);
+                updateLabel();
+            }
+
+            private void updateLabel() {
+                String myFormat = "yyyy-MM-dd";
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+                bt_datepicker.setText(sdf.format(myCalendar.getTime()));
+            }
+
+        };
+
+        bt_datepicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(Lap_aktivitas.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
 
-    }
-    private void showDateDialog(){
 
-        /**
-         * Calendar untuk mendapatkan tanggal sekarang
-         */
-        Calendar newCalendar = Calendar.getInstance();
+        simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tgl = bt_datepicker.getText().toString();
+                String nm = namanya.getText().toString();
 
-        /**
-         * Initiate DatePicker dialog
-         */
-        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                if (tgl.isEmpty()) {
+
+                    bt_datepicker.setError("Masukkan Tanggal");
+
+                } else if (nm.isEmpty()) {
+                    namanya.setError("Masukkan Nama");
 
 
+                } else {
+                    sendData(tgl, nm);
 
-            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
 
-                /**
-                 * Method ini dipanggil saat kita selesai memilih tanggal di DatePicker
-                 */
+                }
 
-                /**
-                 * Set Calendar untuk menampung tanggal yang dipilih
-                 */
-                Calendar newDate = Calendar.getInstance();
-                newDate.set(year, monthOfYear, dayOfMonth);
 
-                /**
-                 * Update TextView dengan tanggal yang kita pilih
-                 */
-                tvDateResult.setText("Tanggal dipilih : "+dateFormatter.format(newDate.getTime()));
             }
+        });
+    }
 
-        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+    public void sendData(final String tanggal, final String nama) {
+        String url = "https://a944b6274200.ngrok.io/tambah-panen";
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                bt_datepicker.setText("");
+                namanya.setText("");
+                Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT).show();
 
-        /**
-         * Tampilkan DatePicker dialog
-         */
-        datePickerDialog.show();
+                Intent menu1 = new Intent(Lap_aktivitas.this, LapAktivitas.class);
+                startActivity(menu1);
+                finish();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        }) {
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                Map<String, String> params = new HashMap<>();
+                params.put("tanggal", tanggal);
+                params.put("nama", nama);
+                return params;
+
+            }
+        };
+        queue.add(request);
     }
 }
